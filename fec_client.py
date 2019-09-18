77a3ed3badbc7b0449931a5c4ba801fa23b01128
Jarkko Kovala <jarkko.kovala@iki.fi>
2019-09-18 10:01:53 +0300

    add comments

--- fec_client.py
+++ fec_client.py
@@ -6,30 +6,36 @@ import struct
 import random
 import time
 
-DEST_IP = "127.0.0.1"
-DEST_PORT = 12345
-DATA_FILE = "E.txt"
-PAYLOAD_SIZE = 100
+# Forward error correction client, sends data as UDP packets
+# Arguments: packet loss probability as a percentage and FEC method (triple redundancy or XOR)
+
+DEST_IP = "127.0.0.1" # Destination IP address
+DEST_PORT = 12345 # Destination UDP address
+DATA_FILE = "E.txt" # Source file where to read data to be sent
+PAYLOAD_SIZE = 100 # How many bytes of payload to send in a packet
 
 if len(sys.argv) < 3 or sys.argv[2] not in ("triple", "xor"):
     print("Usage:", sys.argv[0], " <packet loss percentage> {triple|xor}")
     exit()
 
-PACKET_LOSS = int(sys.argv[1])
-METHOD = sys.argv[2]
+PACKET_LOSS = int(sys.argv[1]) # Packet loss probability (percentage 0-100)
+METHOD = sys.argv[2] # FEC method ("triple" or "xor")
 
-count = 0
-drops = 0
+count = 0 # Packet counter
+drops = 0 # How many packets have been dropped so far
 
+# Construct and try to send a packet simulating packet loss
+# Arguments: socket where to send and payload to send
 def try_send(s, payload):
     global count
     global drops
 
-    if(random.randint(1, 100) > PACKET_LOSS):
+    if(random.randint(1, 100) > PACKET_LOSS): # Randomly drop packets
+        # Construct packet: 4 bytes for packet id plus payload
         packet = struct.pack("!l", count) + payload
 
         s.sendto(packet, (DEST_IP, DEST_PORT))
-        time.sleep(0.001)
+        time.sleep(0.001) # Don't send too fast
     else:
         drops += 1
     
@@ -40,20 +46,26 @@ def main():
         sent = 0
         data = f.read(PAYLOAD_SIZE)
 
+        # Read from data file until no more data.
         while data:
             if METHOD == "triple":
+                # Triple redundancy: just send three copies of packet
                 for i in range(3):
                     try_send(s, data)
                 sent += 1
             else: # METHOD == "xor"
+                # XOR: send two packets plus xor of the two packets
+                # First packet
                 try_send(s, data)
                 sent += 1
 
+                # Second packet (if there is any more data)
                 data2 = f.read(PAYLOAD_SIZE)
                 if(data2):
                     try_send(s, data2)
                     sent += 1
 
+                    # Calculate XOR of the previous two and send
                     xor = bytes(x ^ y for x, y in zip(data, data2))
                     try_send(s, xor)
                 else:
--- fec_server.py
+++ fec_server.py
@@ -5,24 +5,31 @@ import sys
 import struct
 import time
 
-LISTEN_IP = "127.0.0.1"
-LISTEN_PORT = 12345
-TIMEOUT = 5
-PAYLOAD_SIZE = 100
-DATA_FILE = "E.txt"
+# Forward error correction server, receives data as UDP packets
+# Argument: FEC method (triple redundancy or XOR)
+
+LISTEN_IP = "127.0.0.1" # IP address where we listen
+LISTEN_PORT = 12345 # UDP port where we listen
+TIMEOUT = 5 # Timeout after we assume there will be no more packets in the stream
+PAYLOAD_SIZE = 100 # How many bytes of payload we receive in each packet
+DATA_FILE = "E.txt" # Data file to compare received data
 
 if len(sys.argv) < 2 or sys.argv[1] not in ("triple", "xor"):
     print("Usage:", sys.argv[0], "{triple|xor}")
     exit()
 
-METHOD = sys.argv[1]
+METHOD = sys.argv[1] # FEC method ("triple" or "xor")
 
+# Try to 
+# Arguments: receive buffer, packet id of the first packet in the buffer, file for comparing data
 def try_decode(buffer, window_start, f):
     ret = 0
 
     if METHOD == "triple":
+        # Triple redundancy: just check through buffer, if there is a packet we're done
         for item in buffer:
             if item:
+                # Compare against datafile to be sure everything worked
                 f.seek(int(window_start/3) * PAYLOAD_SIZE)
 
                 if item == f.read(PAYLOAD_SIZE):
@@ -30,17 +37,22 @@ def try_decode(buffer, window_start, f):
 
                 break
     else: # METHOD == "xor"
+        # XOR: If packet is missing try to reconstruct from another packet and xor packet
+        # If first packet is missing and we have second and third, reconstruct
         if not buffer[0] and (buffer[1] and buffer[2]):
             buffer[0] = bytes(x ^ y for x, y in zip(buffer[1], buffer[2]))
         
+        # If second packet is missing and we have first and third, reconstruct
         if not buffer[1] and (buffer[0] and buffer[2]):
             buffer[1] = bytes(x ^ y for x, y in zip(buffer[0], buffer[2]))
         
+        # If we now have first packet, compare against datafile
         if buffer[0]:
             f.seek((window_start - int(window_start/3)) * PAYLOAD_SIZE)
             if buffer[0] == f.read(PAYLOAD_SIZE):
                 ret += 1
         
+        # If we now have second packet, compare against datafile
         if buffer[1]:
             f.seek(((window_start - int(window_start/3)) + 1) * PAYLOAD_SIZE)
             if buffer[1] == f.read(PAYLOAD_SIZE):
@@ -60,10 +72,12 @@ def main():
         print("Listening for udp packets at", LISTEN_IP, "port", LISTEN_PORT)
         while True:
             try:
+                # Listen for packets
                 data, addr = s.recvfrom(1024)
                 sys.stdout.write('.')
                 sys.stdout.flush()
             except socket.timeout:
+                # If no packets have been received in TIMEOUT seconds flush buffer and start over
                 print("\nTimeout with", 3 - buffer.count(0), "packets in buffer")
                 if(buffer.count(0) < 3):
                     payload_count += try_decode(buffer, window_start, f)
@@ -73,22 +87,30 @@ def main():
                 payload_count = 0
                 continue
 
+            # We have packet
             recv_count += 1
+            # Extract packet id from the first 4 bytes
             id = struct.unpack_from("!l", data)[0]
 
+            # Extract payload
             payload = data[4:]
+            # Location of received packet in window
             modulus = id % 3
 
+            # If we received packet smaller than current window, assume we have started over
             if id < window_start:
                 window_start = 0
 
+            # If we received packet outside current window (window_start + 2), flush buffer
             if (id - window_start) > 2:
                 payload_count += try_decode(buffer, window_start, f)
                 buffer = [0, 0, 0]
                 window_start = id - modulus
 
+            # Insert packet in buffer
             buffer[modulus] = payload
 
+            # If we received packet at window end, flush buffer
             if modulus == 2:
                 payload_count += try_decode(buffer, window_start, f)
                 buffer = [0, 0, 0]
